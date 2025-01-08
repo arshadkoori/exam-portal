7ebcf262122bed327e6c3bd69d758ded055f3d97
Akash <akashkrishnan806@gmail.com>
2025-01-08 10:35:37 +0530

    First commit

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.static("./dist"));
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json({ limit: "5mb" }));
 
 app.use("/api", router);
 
